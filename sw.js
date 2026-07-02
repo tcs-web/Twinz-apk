83ff5958adc14bdd2bb87c6ba7a7fb62d0459dc2
tcs-web <tbose19651@gmail.com>
2026-07-02 20:29:52 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='twinz-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/', '/index.html', '/offline.html']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/offline.html')))));
