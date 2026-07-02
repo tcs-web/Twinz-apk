c8e493a1a39ca8071852be312b165c168eab8cc3
tcs-web <tbose19651@gmail.com>
2026-07-02 17:41:51 +0800

    Add service worker for caching and offline support

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='twinz-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/', '/index.html', '/offline.html']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/offline.html')))));
